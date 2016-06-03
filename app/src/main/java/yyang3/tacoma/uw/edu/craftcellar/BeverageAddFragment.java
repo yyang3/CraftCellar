a652d44f54bd21b645607284c5c035fc322f880a
yyang3 <yyang3@uw.edu>
2016-06-03 11:49:15 -0700

    Wrap up everything

--- app/src/main/java/yyang3/tacoma/uw/edu/craftcellar/BeverageAddFragment.java
+++ app/src/main/java/yyang3/tacoma/uw/edu/craftcellar/BeverageAddFragment.java
@@ -1,36 +1,23 @@
 package yyang3.tacoma.uw.edu.craftcellar;
 
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Calendar;
 
-import yyang3.tacoma.uw.edu.craftcellar.Beverage.Beverage;
+import java.util.Calendar;
 
 
 /**
